3f2fc6c5ea015446017f677be5fa771302076bfb
A . PERIYANAYAGA RAJ <144892974+Periyanayagaraj@users.noreply.github.com>
2024-10-27 18:08:49 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9q4bq!mux8uf@8u@^@vol)jkq_49ei5xm)9b2a#lfjqgr7k4ho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
